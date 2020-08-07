4056b31822322b756c8379d75449ee63affbf91c
“ <1341289162@qq.com>
2020-08-07 09:28:02 +0800

    路由

---
+++ .history/router/src/commponent/LoyOut/index_20200805192236.js
@@ -0,0 +1,6 @@
+import React, { Component, Fragment } from 'react'
+
+
+export default class LayOut extends Component {
+
+}
---
+++ router/src/component/LayOut/index.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+
+
+export default class LayOut extends Component {
+    render () {
+        return (
+            <Fragment>
+                <div>  你好 </div>
+            </Fragment>
+        )
+    }
+
+}
